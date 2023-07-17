a745846bc9cca3b1319f5cf53a8690ca240ff012
Vladimir Popovic <vpopovic003@gmail.com>
2023-07-17 14:35:10 +0200

    update CV

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -14,7 +14,7 @@ const Hero = () => (
       <SectionText>
         Hello and welcome. This website contains my personal portfolio of projects I worked on. Check out the projects bellow or download my CV. Enjoy!
       </SectionText>
-      <Button onClick={() => window.location = 'https://drive.google.com/file/d/1TzjGIRIjJUiz_ATwS-VrrehIBII3v6zD/view?usp=sharing'}>Download CV</Button>
+      <Button onClick={() => window.location = 'https://drive.google.com/file/d/1J_fzOAlMRHM9V9dRdnjBYSPvoO3uqRJO/view?usp=sharing'}>Download CV</Button>
     </LeftSection>
   </Section>
 );
